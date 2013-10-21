34d6ebf39499fe83172f49648f8e6a40c4f2fb51
Todd Branchflower <todd.branchflower@gmail.com>
2013-10-21 15:38:10 -0600

    improved formatting

--- moving_average.c
+++ moving_average.c
@@ -5,24 +5,25 @@
  *      Author: C15Ryan.Turner
  */
 
-#ifndef MOVING_AVERAGE_C_
-#define MOVING_AVERAGE_C_
-
 #include "moving_average.h"
 
 int getAverage(int array[], unsigned int arrayLength){
     int i;
 	int sum=0;
 	int avg=0;
+
 	for (i = 0; i < arrayLength; i++){
 			sum = array[1];
 	}
+
 	avg = sum/arrayLength;
+
 	return avg;
 }
 
 void addSample(int sample, int array[], unsigned int arrayLength){
 	int i;
+
 	for (i = 0; i < arrayLength - 1; i++) {
 		array[i] = array[i+1];
 	}
@@ -33,27 +34,29 @@ void addSample(int sample, int array[], unsigned int arrayLength){
 int max(int array[], unsigned int arrayLength){
 	int i;
 	int max = array[0];
+
 	for (i = 0; i < arrayLength; i++){
 		if(array[1] < max){
 			max = array[1];
 		}
 	}
+
 	return max;
 }
 
 int min(int array[], unsigned int arrayLength){
 	int i;
 	int min = array[0];
+
 	for (i = 0; i < arrayLength; i++){
 		if(array[1] < min){
 			min = array[1];
 		}
 	}
+
 	return min;
 }
 
 unsigned int range(int array[], unsigned int arrayLength){
     return max(array, arrayLength) - min(array, arrayLength);
 }
-
-#endif /* MOVING_AVERAGE_C_ */
