1b12840c0711ba9ade856abfb86b96bbd31d4c68
Ryan Turner <c15ryan.turner@usafa.edu>
2013-10-20 23:50:01 -0600

    First Commit

---
+++ main.c
@@ -0,0 +1,30 @@
+/* Author: C2C Ryan Turner
+ * Date: 14 Oct 2013
+ * Description: Implements a subset of the pong game
+ */
+
+#include <msp430.h> 
+#include "moving_average.h"
+
+#define N_AVG_SAMPLES 2
+
+/*
+ * main.c
+ */
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+    int array[N_AVG_SAMPLES] = {0};
+    int output = 0;
+	int input[] = {45, 42, 41, 40, 43, 45, 46, 47, 49, 45};
+
+	while (output < sizeof(input) / sizeof(int))
+	{
+	     addSample(input[output], array, N_AVG_SAMPLES);
+	     printf("%d\n", getAverage(array, N_AVG_SAMPLES));
+	     output++;
+	     sleep(1);
+	}
+
+	    return 0;
+}
---
+++ moving_average.h
@@ -0,0 +1,22 @@
+/*
+ * moving_average.h
+ *
+ *  Created on: Oct 16, 2013
+ *      Author: C15Ryan.Turner
+ */
+
+#ifndef MOVING_AVERAGE_H_
+#define MOVING_AVERAGE_H_
+
+// Moving average functions
+int getAverage(int array[], unsigned int arrayLength);
+void addSample(int sample, int array[], unsigned int arrayLength);
+
+// Array functions
+int max(int array[], unsigned int arrayLength);
+int min(int array[], unsigned int arrayLength);
+unsigned int range(int array[], unsigned int arrayLength);
+
+
+
+#endif /* MOVING_AVERAGE_H_ */
